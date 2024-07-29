65f2a31a4d1564ffbf4a15bcf4fb7f2a34075c64
matte <matteplaysmahjong@gmail.com>
2024-07-29 21:29:23 +0100

    Pixel-perfect full-size output in JS

--- game/src/jsMain/kotlin/com/game/template/JsApp.kt
+++ game/src/jsMain/kotlin/com/game/template/JsApp.kt
@@ -1,14 +1,50 @@
 package com.game.template
 
+import com.littlekt.Context
+import com.littlekt.RemoveContextCallback
 import com.littlekt.createLittleKtApp
+import kotlinx.browser.document
+import kotlinx.browser.window
+import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.HTMLElement
+
+private const val CANVAS_ID = "canvas"
 
 fun main() {
     createLittleKtApp {
         width = 960
         height = 540
         title = "LittleKt Game Template"
-        canvasId = "canvas"
+        canvasId = CANVAS_ID
     }.start {
+        scheduleCanvasResize(it)
         Game(it)
+    }
+}
+
+private fun scheduleCanvasResize(context: Context) {
+    var removeContextCallback: RemoveContextCallback? = null
+    removeContextCallback = context.onUpdate {
+        // scale the container to allow canvas take all the available device pixel of hi-DPI display
+        val canvasContainer = document.getElementById("canvas-container") as HTMLElement
+        val zoom = (1 / window.devicePixelRatio).toFloat()
+        canvasContainer.style.apply {
+            this.asDynamic().zoom = "$zoom"
+        }
+
+        // resize the canvas to fit max available space
+        val canvas = document.getElementById(CANVAS_ID) as HTMLCanvasElement
+        canvas.style.apply {
+            display = "block"
+            position = "absolute"
+            top = "0"
+            bottom = "0"
+            left = "0"
+            right = "0"
+            width = "100%"
+            height = "100%"
+        }
+        removeContextCallback?.invoke()
+        removeContextCallback = null
     }
 }
