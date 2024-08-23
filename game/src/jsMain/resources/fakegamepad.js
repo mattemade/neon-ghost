551cc0b9104c4c0928d944762134a9e229d1c257
matte <matteplaysmahjong@gmail.com>
2024-08-23 09:47:36 +0100

    Gamepad stuff

--- game/src/jsMain/resources/fakegamepad.js
+++ game/src/jsMain/resources/fakegamepad.js
@@ -1,5 +1,6 @@
 const gamepadSimulator = {
   getGamepads: null,
+  gamepads: null,
   fakeController: {
     axes: [0, 0, 0, 0],
     buttons: [
@@ -90,7 +91,7 @@ const gamepadSimulator = {
       }
     ],
     connected: false,
-    id: "Standard gamepad by Alvaro Montoro",
+    id: "totally fake game controller for the Neon Ghost game",
     index: 0,
     mapping: "standard",
     timestamp: Math.floor(Date.now() / 1000)
@@ -98,8 +99,18 @@ const gamepadSimulator = {
   create: function () {
     gamepadSimulator.getGamepads = navigator.getGamepads;
     navigator.getGamepads = function () {
-      return gamepadSimulator.getGamepads().concat(gamepadSimulator.fakeController);
+      return gamepadSimulator.gamepads;
     }
+  },
+  getRealGamepads: function () {
+    if (!gamepadSimulator.getGamepads) {
+      return navigator.getGamepads();
+    }
+    let modifiedGetGamepads = navigator.getGamepads;
+    navigator.getGamepads = gamepadSimulator.getGamepads;
+    var realGamepads = navigator.getGamepads();
+    navigator.getGamepads = modifiedGetGamepads;
+    return realGamepads;
   },
   touchButton: function (index) {
     gamepadSimulator.fakeController.buttons[index].touched = true;
@@ -129,11 +140,22 @@ const gamepadSimulator = {
     }
     navigator.getGamepads = gamepadSimulator.getGamepads;
   },
+  updateGamepads: function () {
+    gamepadSimulator.gamepads = gamepadSimulator.getRealGamepads();
+    if (gamepadSimulator.fakeController.index == gamepadSimulator.gamepads.length) {
+      gamepadSimulator.gamepads = gamepadSimulator.concat(gamepadSimulator.fakeController);
+    } else {
+      gamepadSimulator.gamepads[gamepadSimulator.fakeController.index] = gamepadSimulator.fakeController;
+    }
+  },
   connect: function () {
     const event = new Event("gamepadconnected");
     gamepadSimulator.fakeController.connected = true;
+    gamepadSimulator.fakeController.index = gamepadSimulator.getFreeIndex();
     gamepadSimulator.fakeController.timestamp = Math.floor(Date.now() / 1000);
     event.gamepad = gamepadSimulator.fakeController;
+    console.log(`connecting to ${gamepadSimulator.fakeController.index}`);
+    gamepadSimulator.updateGamepads();
     window.dispatchEvent(event);
   },
   disconnect: function () {
@@ -142,7 +164,52 @@ const gamepadSimulator = {
     gamepadSimulator.fakeController.timestamp = Math.floor(Date.now() / 1000);
     event.gamepad = gamepadSimulator.fakeController;
     window.dispatchEvent(event);
+  },
+  getFreeIndex: function () {
+    let realGamepads = gamepadSimulator.getRealGamepads();
+    var index = realGamepads.indexOf(null);
+    if (index < 0) {
+      index = realGamepads.length;
+    }
+    return index;
   }
 }
 
-gamepadSimulator.create();
+window.addEventListener("gamepadconnected", (e) => {
+  let connectedGamepad = navigator.getGamepads()[e.gamepad.index];
+  console.log(`gamepad connected ${connectedGamepad}`);
+
+  let index = gamepadSimulator.getFreeIndex();
+  if (index != gamepadSimulator.fakeController.index) {
+    console.log(`clash at index ${index}, reconnecting`);
+    gamepadSimulator.connect();
+  }
+});
+
+window.addEventListener("gamepaddisconnected", (e) => {
+  console.log(`gamepad disconnected ${e.gamepad}`);
+
+  if (e.index >= gamepadSimulator.fakeController.index) {
+    gamepadSimulator.connect();
+  } else {
+    gamepadSimulator.updateGamepads();
+  }
+});
+
+var userInputEventNames = [
+        'click', 'contextmenu', 'auxclick', 'dblclick', 'mousedown',
+        'mouseup', 'pointerup', 'touchend', 'keydown', 'keyup', 'touchstart'
+    ];
+
+var createAndConnectFakeGamepad = function(e) {
+  gamepadSimulator.create();
+  gamepadSimulator.connect();
+
+  userInputEventNames.forEach(function (eventName) {
+      window.removeEventListener(eventName, createAndConnectFakeGamepad);
+  });
+};
+
+userInputEventNames.forEach(function (eventName) {
+  window.addEventListener(eventName, createAndConnectFakeGamepad);
+});
