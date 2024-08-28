aff7d0ed3058c6403a28720a559fa9ac46930e33
matte <matteplaysmahjong@gmail.com>
2024-08-28 07:49:06 +0100

    Perspective street

--- game/src/commonMain/kotlin/io/itch/mattemade/neonghost/world/Wall.kt
+++ game/src/commonMain/kotlin/io/itch/mattemade/neonghost/world/Wall.kt
@@ -12,7 +12,7 @@ import org.jbox2d.dynamics.Filter
 import org.jbox2d.dynamics.FixtureDef
 import org.jbox2d.dynamics.World
 
-class Wall(world: World, val x: Float, val y: Float, val shape: TiledMap.Object.Shape, mapHeight: Int) {
+class Wall(world: World, val x: Float, val y: Float, val shape: TiledMap.Object.Shape, private val mapHeight: Int) {
 
     private val body = world.createBody(
         BodyDef(
@@ -37,25 +37,26 @@ class Wall(world: World, val x: Float, val y: Float, val shape: TiledMap.Object.
         )
         )
     }
-}
 
-private fun p(x: Float, y: Float) = Vec2(x, y).mulLocal(Game.IPPU)
+    private fun p(x: Float, y: Float) = Vec2(x, y).mulLocal(Game.IPPU)
 
-private fun TiledMap.Object.Shape.toPoints(): Array<Vec2> {
-    return when (this) {
-        is TiledMap.Object.Shape.Rectangle -> {
-            arrayOf(
-                p(0f, 0f),
-                p(width, 0f),
-                p(width, height),
-                p(0f, height),
-            )
+    private fun TiledMap.Object.Shape.toPoints(): Array<Vec2> {
+        return when (this) {
+            is TiledMap.Object.Shape.Rectangle -> {
+                arrayOf(
+                    p(0f, 0f),
+                    p(width, 0f),
+                    p(width, height),
+                    p(0f, height),
+                )
+            }
+
+            is TiledMap.Object.Shape.Ellipse -> TODO()
+            TiledMap.Object.Shape.Point -> TODO()
+            is TiledMap.Object.Shape.Polygon -> points.map { p(it.x,  -it.y) }.toTypedArray()
+            is TiledMap.Object.Shape.Polyline -> points.map { p(it.x, -it.y) }.toTypedArray()
+
+            is TiledMap.Object.Shape.Text -> TODO()
         }
-
-        is TiledMap.Object.Shape.Ellipse -> TODO()
-        TiledMap.Object.Shape.Point -> TODO()
-        is TiledMap.Object.Shape.Polygon -> TODO()
-        is TiledMap.Object.Shape.Polyline -> TODO()
-        is TiledMap.Object.Shape.Text -> TODO()
     }
 }
